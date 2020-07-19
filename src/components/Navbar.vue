6f558f8ae2156e65131d67d0c4fc0002ac9043d1
PolarizedIons <polarizedions@gmail.com>
2020-07-19 23:29:55 +0200

    start of a navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,16 @@
 <template>
-  <nav>
-    <h1
-    >
-      Test
-    </h1>
+  <nav class="bg-gray-800 flex items-center">
+    <div class="flex-1"></div>
+    <div class="flex-1 flex items-center justify-center">
+      <img src="@/assets/img/logo.png" class="h-16 m-3" />
+      <h1 class="text-4xl">Disquotes</h1>
+    </div>
+    <ul class="flex-1 flex justify-end h-24">
+      <li class="h-full"><a class="inline-block h-full px-3 hover:bg-gray-700 flex items-center" href="#">Add the bot!</a></li>
+      <li class="h-full"><router-link class="inline-block h-full px-3 hover:bg-gray-700 flex items-center" :to="{ name: 'login' }">Login</router-link></li>
+      <li class="h-full"><router-link class="inline-block h-full px-3 hover:bg-gray-700 flex items-center" :to="{ name: 'dashboard' }">Dashboard</router-link></li>
+      <li class="h-full"><router-link class="inline-block h-full px-3 hover:bg-gray-700 flex items-center" :to="{ name: 'unmoderated' }">Unmoderated Quotes</router-link></li>
+    </ul>
   </nav>
 </template>
 
