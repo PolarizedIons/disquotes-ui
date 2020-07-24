947b997e4c629ff4f8996ac6c9ad8a96300efa66
PolarizedIons <polarizedions@gmail.com>
2020-07-24 21:08:13 +0200

    add forgotten loader on view quote page

--- src/views/ViewQuote.vue
+++ src/views/ViewQuote.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="mx-auto w-3/4 my-5 flex justify-between">
     <single-quote v-if="quote" :quote="quote" class="w-full"></single-quote>
+    <div v-else class="w-full flex justify-center">
+      <loader></loader>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import SingleQuote from "@/components/SingleQuote.vue";
+import Loader from "@/components/Loader.vue";
 import { Quote } from "../models/Quote";
 import QuoteService from "../services/QuoteService";
 
 @Component({
   components: {
-    SingleQuote
+    SingleQuote,
+    Loader
   }
 })
 export default class ViewQuote extends Vue {
